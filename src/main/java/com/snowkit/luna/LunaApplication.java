a7a73e365fedcb43c9f3e6be58396175cebbcc1c
snowkit-1 <snowkit100@gmail.com>
2022-03-30 13:20:20 +0900

    Start project

---
+++ src/main/java/com/snowkit/luna/LunaApplication.java
@@ -0,0 +1,13 @@
+package com.snowkit.luna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LunaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LunaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/snowkit/luna/LunaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.snowkit.luna;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LunaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
