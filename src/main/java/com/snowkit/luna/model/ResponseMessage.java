fea29ebf2c04870c149f72ad4bc8028db57d2124
snowkit-1 <snowkit100@gmail.com>
2022-03-31 12:14:23 +0900

    Feat: Add DTO

---
+++ src/main/java/com/snowkit/luna/model/ResponseMessage.java
@@ -0,0 +1,15 @@
+package com.snowkit.luna.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class ResponseMessage {
+
+    private String message;
+}
