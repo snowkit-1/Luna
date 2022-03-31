b50a33218a87d76786ed2c46211e1cd6700ba1b1
snowkit-1 <snowkit100@gmail.com>
2022-03-31 12:13:51 +0900

    Feat: Add ObjectMapper Utility

---
+++ src/main/java/com/snowkit/luna/util/ObjectMapperUtil.java
@@ -0,0 +1,27 @@
+package com.snowkit.luna.util;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.MapperFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.json.JsonMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
+import java.util.TimeZone;
+
+public class ObjectMapperUtil {
+
+    private static final ObjectMapper objectMapper;
+
+    static {
+        objectMapper = JsonMapper.builder()
+                .enable(MapperFeature.ACCEPT_CASE_INSENSITIVE_PROPERTIES)
+                .disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
+                .build();
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.setTimeZone(TimeZone.getTimeZone("Asia/Seoul"));
+    }
+
+    public static ObjectMapper get() {
+        return objectMapper;
+    }
+}
