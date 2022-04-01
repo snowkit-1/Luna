a51e97d6ad29aded8b2a8f732c8e8e591ed1fee6
snowkit-1 <snowkit100@gmail.com>
2022-04-01 23:36:16 +0900

    Feat: [Spring Security] AuthenticationEntryPoint 구현 (인증되지 않은 경우)

---
+++ src/main/java/com/snowkit/luna/config/security/CustomAuthenticationEntryPoint.java
@@ -0,0 +1,22 @@
+package com.snowkit.luna.config.security;
+
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response,
+                         AuthenticationException authException) {
+
+        throw new AccessDeniedException("인증 토큰이 없습니다.");
+    }
+}
