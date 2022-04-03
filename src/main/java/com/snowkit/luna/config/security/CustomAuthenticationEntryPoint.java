d3195a83eb4f612fd1b296657604f771da530d12
snowkit-1 <snowkit100@gmail.com>
2022-04-04 04:28:23 +0900

    Feat: Optimize import

--- src/main/java/com/snowkit/luna/config/security/CustomAuthenticationEntryPoint.java
+++ src/main/java/com/snowkit/luna/config/security/CustomAuthenticationEntryPoint.java
@@ -5,10 +5,8 @@ import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 @Component
 public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
